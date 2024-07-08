0586864264519cd0a3558f1e42428a7650e0b263
Naira <nkazeian@gmail.com>
2024-07-08 17:32:00 +0300

    apply-operation

--- Leetcode/2460-apply-operation-on-array/main.go
+++ Leetcode/2460-apply-operation-on-array/main.go
@@ -1,12 +1,29 @@
-package applyoperationonarray
+package main
 
 func applyOperations(nums []int) []int {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == nums[i+1] {
-			nums[i] = nums[i] * 2
-			nums[i+1] = 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			nums[i-1] = nums[i] * 2
+			nums[i] = 0
 		}
-
+	}
+	//переміщуємо всі ненульові значення на початок масиву
+	lastNonZeroFoundAt := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[lastNonZeroFoundAt] = nums[i]
+			if lastNonZeroFoundAt != i {
+				nums[i] = 0
+			}
+			lastNonZeroFoundAt++
+		}
 	}
 	return nums
 }
+
+func main() {
+	applyOperations([]int{1, 2, 2, 1, 1, 0})
+
+}
+
+// https://leetcode.com/problems/apply-operations-to-an-array/
