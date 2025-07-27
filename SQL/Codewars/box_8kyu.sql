759ee3a893923aef0c632096c80f319dc3d42641
Naira Kazeian <nkazeian@gmail.com>
2025-07-27 23:38:40 +0300

    concatening_7kyu

--- SQL/Codewars/box_8kyu.sql
+++ SQL/Codewars/box_8kyu.sql
@@ -1,9 +1,5 @@
---task "Surface Area and Volume of a Box"
-
--- # write your SQL statement here: 
--- you are given a table 'box' with columns: width (int), height (int), depth (int)
--- return a query with columns: width, height, depth, area (int),  as volume (int)
--- sort results by area ascending, then volume ascending, then width ascending, then height ascending
+-- Surface Area and Volume of a Box
+-- https://www.codewars.com/kata/565f5825379664a26b00007c
 
 SELECT WIDTH, HEIGHT, DEPTH, 2 * (WIDTH * HEIGHT + DEPTH * WIDTH + DEPTH * HEIGHT) AS AREA, WIDTH * HEIGHT * DEPTH AS VOLUME
 FROM BOX
---
+++ SQL/Codewars/concatening_7kyu.sql
@@ -0,0 +1,5 @@
+-- SQL: Concatenating Columns
+-- https://www.codewars.com/kata/59440034e94fae05b2000073/train/sql
+
+SELECT CONCAT(prefix, ' ', first, ' ',last, ' ', suffix) AS title
+FROM names;
